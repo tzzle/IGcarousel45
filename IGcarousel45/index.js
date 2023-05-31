f7a1c9b637275e36d42021c178df573edc743bf7
tzzle <130739462+tzzle@users.noreply.github.com>
2023-05-31 14:01:39 +0300

    Add files via upload

---
+++ IGcarousel45/index.js
@@ -0,0 +1,7 @@
+function bulbOn() {
+    document.getElementById("bulb").src = 'images/pic_bulbon.gif';
+}
+
+function bulbOff() {
+    document.getElementById("bulb").src = 'images/pic_bulboff.gif';
+}
